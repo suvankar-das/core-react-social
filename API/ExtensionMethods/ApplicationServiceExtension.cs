0b6a6c7bef0153ca46b32b047ac841ba3fe544ac
Suvankar Das <hi.suvankar@gmail.com>
2024-11-19 17:21:40 +0530

    sss

--- API/ExtensionMethods/ApplicationServiceExtension.cs
+++ API/ExtensionMethods/ApplicationServiceExtension.cs
@@ -1,11 +1,42 @@
-﻿namespace API.ExtensionMethods
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.UnitOfWorks;
+
+namespace API.ExtensionMethods
 {
     public static class ApplicationServiceExtension
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration configuration)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration configuration,string CORS_NAME,string ALLOWED_HOST)
         {
-
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+           
+
+            services.AddDbContext<ApplicationDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
+            });
+
+
+            services.AddCors(option =>
+            {
+                option.AddPolicy(CORS_NAME, policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(ALLOWED_HOST);
+                });
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            // mediatr
+            services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
+            // add auto mapper service
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,43 +1,19 @@
-using Application.Activities;
-using Application.Core;
+using API.ExtensionMethods;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using Persistence.UnitOfWorks;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 string CORS_NAME = "CorsPolicy";
 string ALLOWED_HOST = "http://localhost:3000";
 
 
-builder.Services.AddDbContext<ApplicationDbContext>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
-});
-
-
-builder.Services.AddCors(option =>
-{
-    option.AddPolicy(CORS_NAME, policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(ALLOWED_HOST);
-    });
-});
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-// mediatr
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
-// add auto mapper service
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+// using extension method
+builder.Services.AddApplicationServices(builder.Configuration, CORS_NAME,ALLOWED_HOST);
 
 var app = builder.Build();
 
@@ -47,8 +23,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-
 
 //app.UseHttpsRedirection();
 app.UseCors(CORS_NAME);
